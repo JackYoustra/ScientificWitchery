0f0a237cd3ce920b75a4b78b1fdcee28c408f3e9
Jack Youstra <jack@youstra.com>
2022-11-25 22:41:22 -0500

    Change icon

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -11,7 +11,7 @@ const siteMetadata = {
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: '/static/images/logo.png',
-  image: '/static/images/avatar.png',
+  image: '/static/images/avatar.webp',
   socialBanner: '/static/images/twitter-card.jpeg',
   email: 'jack@youstra.com',
   github: 'https://github.com/jackyoustra',
