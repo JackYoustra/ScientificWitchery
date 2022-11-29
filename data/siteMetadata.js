8a93257efca307162ec82e5b1fef0a2cb72667c1
Jack Youstra <jack@youstra.com>
2022-11-28 23:39:37 -0800

    Change site url and repo

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -8,8 +8,8 @@ const siteMetadata = {
   description: 'Writings on my projects and cool language features.',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://ScientificWitchery.vercel.app',
+  siteRepo: 'https://github.com/JackYoustra/ScientificWitchery',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.webp',
   socialBanner: '/static/images/twitter-card.jpeg',
