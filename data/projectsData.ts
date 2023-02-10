7c3762fadad9b0d62119e07a447c0f9f0c0bcaa0
Jack Youstra <jack@youstra.com>
2023-02-09 17:25:16 -0800

    Add project

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -6,6 +6,12 @@ const projectsData = [
     href: 'https://www.nanoflick.com',
   },
   {
+    title: 'WTF Happened in 1791?',
+    description: `After reading Slouching Towards Utopia, and reading WTF Happened in 1971 one too many times, I decided to make a parody site for the year 1791. It turns out you only need one graph!`,
+    imgSrc: '/static/images/1791.webp',
+    href: 'https://www.wtfhappenedin1791.com',
+  },
+  {
     title: 'Victoria 2 savegame analyzer',
     description: `A tool to analyze your Victoria 2 savegames.
     You can view the status of your factories and pops, and synthesize wikiboxes for every war in the game.
