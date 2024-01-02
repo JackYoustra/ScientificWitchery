bf83e4ed6c1ffdebf7ae975d75956e271a1eacf6
Jack Youstra <jack@youstra.com>
2024-01-01 23:12:52 -0800

    Nice basic test working

--- rust-wasm/src/lib.rs
+++ rust-wasm/src/lib.rs
@@ -3,7 +3,9 @@ mod utils;
 use jomini::json::DuplicateKeyMode::Preserve;
 use jomini::json::JsonOptions;
 use jomini::TextTape;
+use twiggy_opt::CommonCliOptions;
 use wasm_bindgen::prelude::*;
+use twiggy_parser;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -23,4 +25,14 @@ pub fn parse_jomini(s: &str) -> Result<String, JsError> {
                 .with_duplicate_keys(Preserve)
         );
     Ok(json.to_string())
+}
+
+#[wasm_bindgen]
+pub fn parse_wasm_binary(s: &[u8]) -> Result<String, JsError> {
+    let mut items = twiggy_parser::parse(&s).map_err(|e| JsError::new(&format!("Error parsing: {}", e)))?;
+    let options = twiggy_opt::Top::default();
+    let top = twiggy_analyze::top(&mut items, &options).map_err(|e| JsError::new(&format!("Error analyzing: {}", e)))?;
+    let mut json = Vec::new();
+    top.emit_json(&items, &mut json).map_err(|e| JsError::new(&format!("Error emitting: {}", e)))?;
+    Ok(String::from_utf8(json).unwrap())
 }
--- rust-wasm/tests/web.rs
+++ rust-wasm/tests/web.rs
@@ -3,11 +3,29 @@
 #![cfg(target_arch = "wasm32")]
 
 extern crate wasm_bindgen_test;
+
+use wasm_bindgen::JsValue;
 use wasm_bindgen_test::*;
+use rust_wasm::parse_wasm_binary;
 
 wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
 fn pass() {
+    assert_eq!(1 + 1, 2);
+}
+
+#[wasm_bindgen_test]
+fn parse_file() {
+    // lol
+    let data = include_bytes!("../pkg/rust_wasm_bg.wasm");
+    let result = parse_wasm_binary(data)
+        .map_err(JsValue::from)
+        .unwrap();
+    // pretty-print the result json
+    let json = js_sys::JSON::parse(&result).unwrap();
+    let pretty = js_sys::JSON::stringify_with_replacer_and_space(&json, &JsValue::null(), &JsValue::from(4i32)).unwrap();
+    // console_log!("result: {}", pretty);
+    console_log!("json: {}", result);
     assert_eq!(1 + 1, 2);
 }
