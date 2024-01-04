0d5b7e7ebf38eafc2e5b8f8293414f221afbcc9d
Jack Youstra <jack@youstra.com>
2024-01-04 02:22:28 -0800

    Restore vercel build system

---
+++ vercel_build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Installing Rustup..."
+# Install Rustup (compiler)
+# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain nightly --target wasm32-unknown-unknown -y
+$(curl -sSf https://sh.rustup.rs | sh -s -- --default-toolchain nightly --target wasm32-unknown-unknown -y) || true
+# Adding binaries to path
+source "$HOME/.cargo/env"
+echo "Installing wasm-pack..."
+# Install wasm-pack (wasm compiler)
+curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y
+# Actually build the thing in a sibling directory named "rust-wasm" in a subshell
+# so we don't pollute the current directory
+(
+  cd rust-wasm
+  echo "Building Rust..."
+  wasm-pack build
+)
