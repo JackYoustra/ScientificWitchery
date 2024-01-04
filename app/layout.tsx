7c62c6014cc7a1054ea274c54c09595478e09303
Jack Youstra <jack@youstra.com>
2024-01-04 01:59:12 -0800

    Fix up tag gore and change flexbox to use all of screen on custom pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -77,10 +77,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <ThemeProviders>
           <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
           <SectionContainer>
-            <div className="flex h-screen flex-col justify-between font-sans">
+            <div className="flex min-h-screen flex-col justify-between font-sans">
               <SearchProvider searchConfig={siteMetadata.search as SearchConfig}>
                 <Header />
-                <main className="mb-auto">{children}</main>
+                <main className="flex flex-col mb-auto h-full grow">{children}</main>
               </SearchProvider>
               <Footer />
             </div>
